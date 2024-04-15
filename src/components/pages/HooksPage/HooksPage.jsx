f9d6258286dd27cda5589e818f10764996b9a4b0
Ameer Ameen <ameerapex496@gmail.com>
2024-04-15 16:19:08 +0200

    added styling and more

--- src/components/pages/HooksPage/HooksPage.jsx
+++ src/components/pages/HooksPage/HooksPage.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import UseStateHook from './components/UseStateHook.jsx'
-import { StyledDiv, StyledHooksWrapper } from '../../shared/StyledComponent.jsx'
+import {
+  StyledDiv,
+  StyledHooksWrapper,
+  StyledTitle,
+} from '../../shared/StyledComponent.jsx'
 
 const HooksPage = () => {
   return (
     <StyledDiv>
-      <div>
-        <h1>this is hooks page </h1>
+      <div className="bg-black">
+        <StyledTitle>this is hooks page </StyledTitle>
       </div>
 
       <div className=" w-[90%] h-[90%] flex flex-row text-start items-start justify-start">
