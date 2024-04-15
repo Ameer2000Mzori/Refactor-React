672ddfce881ffd8af4ab0d9d160f4fd83456a046
Ameer Ameen <ameerapex496@gmail.com>
2024-04-15 16:07:00 +0200

    added logic and using tailwinds styling

--- src/components/pages/HooksPage/HooksPage.jsx
+++ src/components/pages/HooksPage/HooksPage.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import UseStateHook from './components/UseStateHook.jsx'
+import { StyledDiv } from './StyledComponent.jsx'
 
 const HooksPage = () => {
   return (
-    <div>
-      <h1>this is hooks page </h1>
-
+    <StyledDiv>
       <div>
-        <UseStateHook />
+        <h1>this is hooks page </h1>
       </div>
-    </div>
+
+      <div className=" w-[90%] h-[90%] flex flex-row text-start items-start justify-start">
+        <div>
+          <UseStateHook />
+        </div>
+      </div>
+    </StyledDiv>
   )
 }
 
---
+++ src/components/pages/HooksPage/StyledComponent.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const StyledDiv = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  align-items: center;
+  justify-content: start;
+  gap: 5px;
+`
