8f2c2b9e456bf629ee3a3808c0d27ce570ab2727
Ameer Ameen <ameerapex496@gmail.com>
2024-04-16 22:38:15 +0200

    added some styling and more

--- src/components/pages/HooksPage/components/useReducerHook.jsx
+++ src/components/pages/HooksPage/components/useReducerHook.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import { StyledButton, StyledTitle } from '../../../shared/StyledComponent'
 
 const useReducerHook = () => {
-  return <div>useReducerHook</div>
+  return (
+    <>
+      <StyledTitle>UseState</StyledTitle>
+      <div>
+        <StyledButton>increment</StyledButton>
+        <StyledButton>decrement</StyledButton>
+      </div>
+      <StyledButton>rest</StyledButton>
+      <StyledTitle></StyledTitle>
+    </>
+  )
 }
 
 export default useReducerHook
