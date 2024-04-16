644af85d1ed50b960493efeafca171851e2ab1a0
Ameer Ameen <ameerapex496@gmail.com>
2024-04-16 22:23:15 +0200

    added some changes to about page

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
-import { StyledDiv } from '../shared/StyledComponent'
+import { StyledDiv, StyledTitle } from '../shared/StyledComponent'
 
 const About = () => {
-  return <StyledDiv>About</StyledDiv>
+  return (
+    <StyledDiv className="bg-black">
+      <StyledTitle className=" max-w-[500px]">
+        this project is made to refactor everything i have learned and i know
+        from react using ReactQuery, React Redux Toolkit and use of packages
+        like Axios and more.
+      </StyledTitle>
+    </StyledDiv>
+  )
 }
 
 export default About
