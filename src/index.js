ac579f33f1e476477fa8aa8511d25221a471dd23
Ameer Ameen <ameerapex496@gmail.com>
2024-04-17 22:37:28 +0200

    added react toolkit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Provider } from 'react-redux'
+import { store } from './store'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const queryClient = new QueryClient()
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </Provider>
+)
