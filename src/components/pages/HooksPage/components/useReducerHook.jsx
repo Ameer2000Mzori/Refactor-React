101470930af1c3a0d816f3261717aa2e087cd35d
Ameer Ameen <ameerapex496@gmail.com>
2024-04-17 13:33:15 +0200

    added some more logic about useReducer

--- src/components/pages/HooksPage/components/useReducerHook.jsx
+++ src/components/pages/HooksPage/components/useReducerHook.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useReducer, useState } from 'react'
 import { StyledButton, StyledTitle } from '../../../shared/StyledComponent'
 
+const reducer = (state, action) => {
+  if (action.type === 'CALL_NAME') {
+    console.log(action.payload)
+    return state
+  }
+  console.log('There is no name input')
+  return state
+}
+
 const useReducerHook = () => {
+  const [newName, setNewName] = useState('test name')
+  const [state, dispatch] = useReducer(reducer, newName)
+
+  const changeName = (userName) => {
+    dispatch({ type: 'CALL_NAME', payload: userName })
+  }
+
   return (
     <>
-      <StyledTitle>UseState</StyledTitle>
-      <div>
-        <StyledButton>increment</StyledButton>
-        <StyledButton>decrement</StyledButton>
-      </div>
-      <StyledButton>rest</StyledButton>
-      <StyledTitle></StyledTitle>
+      <StyledTitle>useReducer</StyledTitle>
+
+      <StyledButton onClick={() => changeName(newName)}>rest</StyledButton>
+      <StyledTitle>{newName}</StyledTitle>
     </>
   )
 }
